9b4225f23a6e02399ecae60151c7a82f53a846f3
nguyenvuthuduyen2006-stack <nguyenvuthuduyen2006@gmail.com>
2026-06-03 11:14:04 +0700

    Add files via upload

--- them.php
+++ them.php
@@ -1,47 +1,57 @@
 <?php
-include "database.php";
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "quanlysinhvien"
+);
 
-$thongbao = "";
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+}
 
 if (isset($_POST['btnThem'])) {
 
-    $msv = trim($_POST['msv']);
-    $hoten = trim($_POST['hoten']);
+    $msv = $_POST['msv'];
+    $hoten = $_POST['hoten'];
     $php = $_POST['php'];
     $mysql = $_POST['mysql'];
     $html = $_POST['html'];
 
     if (
         $msv == "" || $hoten == "" ||
-        $php === "" || $mysql === "" || $html === ""
+        $php == "" || $mysql == "" || $html == ""
     ) {
         $thongbao = "Không được để trống!";
     }
-    elseif (
-        $php < 0 || $php > 10 ||
-        $mysql < 0 || $mysql > 10 ||
-        $html < 0 || $html > 10
-    ) {
-        $thongbao = "Điểm phải từ 0 - 10!";
-    }
     else {
 
-        $check = mysqli_query($conn, "SELECT * FROM sinhvien WHERE msv='$msv'");
+        $check = mysqli_query($conn,
+            "SELECT * FROM sinhvien WHERE msv='$msv'"
+        );
 
         if (mysqli_num_rows($check) > 0) {
             $thongbao = "MSV đã tồn tại!";
         } else {
 
-            mysqli_query($conn,
-                "INSERT INTO sinhvien VALUES('$msv','$hoten','$php','$mysql','$html')"
-            );
+            $sql = "INSERT INTO sinhvien(msv,hoten,php,mysql,html)
+                    VALUES('$msv','$hoten','$php','$mysql','$html')";
 
-            $thongbao = "Thêm thành công!";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                header("Location: index.php");
+                exit();
+            } else {
+                $thongbao = "Lỗi: " . mysqli_error($conn);
+            }
         }
     }
 }
-
-echo $thongbao;
 ?>
-<br>
-<a href="index.php">Xem danh sách</a>
+
+<p style="color:red;">
+    <?= $thongbao ?>
+</p>
+
+<a href="from_them.html">Quay lại</a>
